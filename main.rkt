b64b1ac0821cc639f018ce4d6201f451e47080c4
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2018-11-05 14:35:12 -0200

    quase pronto, falta repetition(sem emoção não tem graça)
    e ver se o evaluate ta 10/10

--- main.rkt
+++ main.rkt
@@ -12,58 +12,96 @@
                                        (edge (node "C") (node "A") "b"))))
 
 (define (next-world world atomic graph [lst '()])
-      (if (null? (graph-struct-l graph))
-          lst
-          (let*
-              ([first-transition (car (graph-struct-l graph))]
-               [origin (edge-origin first-transition)]
-               [destination (node-name (edge-destination first-transition))]
-               [label (edge-label first-transition)])
-            (if (and
-                 (equal? world (node-name origin))
-                 (equal? (atomic-a atomic) label))
-                (next-world world atomic (graph-struct (cdr (graph-struct-l graph))) (cons destination lst))
-                (next-world world atomic (graph-struct (cdr (graph-struct-l graph))) lst)))))
+  (if (null? (graph-struct-l graph))
+      lst
+      (let*
+          ([first-transition (car (graph-struct-l graph))]
+           [origin (edge-origin first-transition)]
+           [destination (node-name (edge-destination first-transition))]
+           [label (edge-label first-transition)])
+        (if (and
+             (equal? world (node-name origin))
+             (equal? (atomic-a atomic) label))
+            (next-world world atomic (graph-struct (cdr (graph-struct-l graph))) (cons destination lst))
+            (next-world world atomic (graph-struct (cdr (graph-struct-l graph))) lst)))))
+
+(define (next-world++ world atomic graph [lst-vertices '()] [lst-arestas '()])
+  (if (null? (graph-struct-l graph))
+      (values lst-vertices lst-arestas) 
+      (let*
+          ([first-transition (car (graph-struct-l graph))]
+           [origin (edge-origin first-transition)]
+           [destination (node-name (edge-destination first-transition))]
+           [label (edge-label first-transition)])
+        (if (and
+             (equal? world (node-name origin))
+             (equal? (atomic-a atomic) label))
+            (next-world++ world atomic (graph-struct (cdr (graph-struct-l graph))) (cons destination lst-vertices) (cons first-transition lst-arestas))
+            (next-world++ world atomic (graph-struct (cdr (graph-struct-l graph))) lst-vertices lst-arestas)))))
+
 
 (define (graph-vertices g)
   (match g
     [(graph-struct l) (remove-duplicates (apply append (map (lambda (e) (list (node-name (edge-origin e)) (node-name (edge-destination e)))) l)))]))
+
+(define (graph-arestas g)
+  (match g
+    [(graph-struct l) l]))
 
 (define (sublist l1 l2)
   (if (null? l1)
       #t
       (and (member (car l1) l2) (sublist (cdr l1) l2))))
 
+
+;(world pdl graph . -> . (values mundos arestas))
+;; proposições atomicas deve retornar (values mundos arestas)
+;; dentro de cada match, todo let deve virar um let-values
+;; para pegar a lista de mundos e de arestas
+;; ai vai retornar as duas(talvez concatenando)
+;; Esse é o roteiro final, o racket contra-ataca
+;; não existe tentativa, faça ou não-faça(sua nota depende disso
+;; então faça)
+
+;; tudo é possivel, o impossivel só demora mais tempo ou é mais caro
+;; exceto o problema da parada.
+
 (define (graph-walker world pdl graph)
   (match pdl
     [(? atomic? a)
-     (let ([new (next-world world pdl graph)])
-           new)]
+     (next-world++ world pdl graph)]
     [(non-deterministic-choice a b)
-     (let ([new-a (graph-walker world a graph)]
-           [new-b (graph-walker world b graph)])
-                   (append new-a new-b))]
+     (let-values ([(new-a new-arestas-a) (graph-walker world a graph)]
+                  [(new-b new-arestas-b) (graph-walker world b graph)])
+       (values (append new-a new-b) (append new-arestas-a new-arestas-b)))]
            
     [(seq a b)
-     (let ([new-a (graph-walker world a graph)]
-           [new-f (lambda (world) (graph-walker world b graph))])
-       (append new-a (apply append (map new-f new-a))))]
+     (let*-values ([(new-a new-arestas-a) (graph-walker world a graph)]
+                   [(new-f) (lambda (world) (graph-walker world b graph))]
+                   [(worlds-and-arestas)
+                    (map
+                     (lambda (world)
+                       (let-values ([(l k) (new-f world)]) (list l k)))
+                     new-a)]
+                   [(worlds) (map car worlds-and-arestas)]
+                   [(arestas) (map cadr worlds-and-arestas)])
+       (values (append new-a (apply append worlds)) (append new-arestas-a (apply append arestas))))]
     [(repetition a) (let rep ([l (list world)]
                               [world world])
                       (let ([new-worlds (graph-walker world a graph)])
                         (if (sublist new-worlds l)
                             l
-(apply append (map (lambda (w) (rep (cons w (append new-worlds l)) w)) new-worlds)))))]))
+                            (apply append (map (lambda (w) (rep (cons w (append new-worlds l)) w)) new-worlds)))))]))
 
 (define (next-world* world pdl graph)
   (match pdl
     [(? atomic? a)
      (let ([new (next-world world pdl graph)])
-           new)]
+       new)]
     [(non-deterministic-choice a b)
      (let ([new-a (next-world* world a graph)]
            [new-b (next-world* world b graph)])
-                   (append new-a new-b))]
+       (append new-a new-b))]
            
     [(seq a b)
      (let ([new-a (next-world* world a graph)]
@@ -78,11 +116,13 @@
 
 
 (define (evaluate pdl world [graph test-graph])
-  (let ([l (cons world (graph-walker world pdl graph))])
-    (if
-     (and
-      (sublist (graph-vertices graph) (remove-duplicates l))
-      (not(null?(next-world* world pdl graph))))
-     #t
-     (last l))))
+  (let-values ([(l k) (graph-walker world pdl graph)])
+    (let ([l (cons world l)])
+      (if
+       (and
+        (sublist (graph-vertices graph) (remove-duplicates l)) ;andei por todos os vertices?
+        (not (null? (next-world* world pdl graph))) ;deu errado o pdl???
+        (sublist (graph-arestas graph) (remove-duplicates k))) ; andei por todas as arestas?
+       #t
+       (last l)))))
 
